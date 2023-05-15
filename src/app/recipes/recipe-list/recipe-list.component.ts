3dbdfa90a3c254a1107a4c19a9f5b7170703d3aa
ssn <shaamnatheshwar@gmail.com>
2023-05-15 13:32:46 +0530

    done with basic navigation

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+
+  recipes : Recipe[] = [
+    new Recipe('Biriyaani','It looks and tastes greate','https://images.unsplash.com/photo-1682685797818-c9dc151d241e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60')
+  ]
+}
